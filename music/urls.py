ab0326ce6af80c5d944152e88230c3d5318ec351
iamabhishek0 <abhi24061997@gmail.com>
2018-12-18 22:30:48 +0530

    delete view

---
+++ music/urls.py
@@ -0,0 +1,26 @@
+from music import views
+from django.urls import path
+from django.conf.urls import url
+
+app_name='music'
+
+urlpatterns = [
+
+    # /music/
+    path("", views.IndexView.as_view(), name = "index"),
+
+    # /music/album_id/
+    path('<int:pk>/', views.Detailview.as_view(), name = "detail"),
+
+
+    #/music/<album_id>/favorite/
+    #path('<int:album_id>/favorite/', views.favorite, name = "favorite"),
+
+    path('album/add/', views.AlbumCreate.as_view(), name='album-add'),
+
+    #/music/album/2/
+    path('album/<int:pk>/', views.AlbumUpdate.as_view(), name='album-update'),
+
+    #/music/album/2/delete/
+    path('album/<int:pk>/delete/', views.AlbumDelete.as_view(), name='album-delete'),
+]
